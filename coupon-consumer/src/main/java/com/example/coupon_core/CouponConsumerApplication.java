cfa2b24ffdab9a3792906cf2b59826297b4f8f68
minutaurus <made_power@naver.com>
2024-07-14 14:44:20 +0900

    공통 의존성 추가
    멀티 모듈에서 공통적으로 사용하는 설정

--- coupon-consumer/src/main/java/com/example/coupon_core/CouponConsumerApplication.java
+++ coupon-consumer/src/main/java/com/example/coupon_core/CouponConsumerApplication.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Import;
 
 @Import(CouponCoreConfiguration.class)
 @SpringBootApplication
-public class CouponCoreApplication {
+public class CouponConsumerApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.config.name","application-core,application-consumer");
-		SpringApplication.run(CouponCoreApplication.class, args);
+		SpringApplication.run(CouponConsumerApplication.class, args);
 	}
 
 }
