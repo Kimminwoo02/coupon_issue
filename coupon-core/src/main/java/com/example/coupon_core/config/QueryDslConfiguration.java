5ea0e5b446705fb6af236ca45d67ffaf6bcc0a50
minutaurus <made_power@naver.com>
2024-07-19 13:04:54 +0900

    QueryDsl Configuration 추가

---
+++ coupon-core/src/main/java/com/example/coupon_core/config/QueryDslConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.coupon_core.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfiguration {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
