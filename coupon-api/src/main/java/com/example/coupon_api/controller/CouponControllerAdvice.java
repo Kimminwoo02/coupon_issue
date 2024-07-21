ffe44731512cbe46895aeed312bd67e3714492c0
minutaurus <made_power@naver.com>
2024-07-21 13:43:22 +0900

    공통 에러를 처리하기 위한 Advice

---
+++ coupon-api/src/main/java/com/example/coupon_api/controller/CouponControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.example.coupon_api.controller;
+
+import com.example.coupon_api.controller.dto.CouponIssueResponseDto;
+import com.example.coupon_core.exception.CouponIssueException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CouponControllerAdvice {
+    @ExceptionHandler(CouponIssueException.class)
+    public CouponIssueResponseDto couponExceptionHandler(CouponIssueException exception){
+        return new CouponIssueResponseDto()
+    }
+
+}
