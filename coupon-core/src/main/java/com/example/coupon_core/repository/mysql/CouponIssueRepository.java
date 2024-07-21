a91ffd06772417b04ce9815703de536c5f765230
minutaurus <made_power@naver.com>
2024-07-21 13:44:53 +0900

    CouponQueryDsl 생성

--- coupon-core/src/main/java/com/example/coupon_core/repository/mysql/CouponIssueRepository.java
+++ coupon-core/src/main/java/com/example/coupon_core/repository/mysql/CouponIssueRepository.java
@@ -1,21 +1,20 @@
 package com.example.coupon_core.repository.mysql;
 
 import com.example.coupon_core.model.CouponIssue;
+import com.example.coupon_core.model.QCouponIssue;
 import com.querydsl.jpa.JPQLQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import  static com.example.coupon_core.model.QCouponIssue.couponIssue;
 
 @RequiredArgsConstructor
 @Repository
 public class CouponIssueRepository {
     private final JPQLQueryFactory queryFactory;
 
-    public CouponIssue findFirstCouponIssue(long couponId,long userId){
-        return queryFactory.selectFrom(couponIssue)
-                .where(couponIssue.couponId.eq(couponId))
-                .where(couponIssue.userId.eq(userId))
+    public CouponIssue findFirstCouponIssue(long couponId, long userId){
+        return queryFactory.selectFrom(QCouponIssue.couponIssue)
+                .where(QCouponIssue.couponIssue.couponId.eq(couponId))
+                .where(QCouponIssue.couponIssue.userId.eq(userId))
                 .fetchFirst();
     }
 
