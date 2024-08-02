5b800345ba3e5a04707296a1427b45f04d4f9e34
minutaurus <made_power@naver.com>
2024-08-02 22:06:23 +0900

    Redis에 사용할 DTO 생성

---
+++ coupon-core/src/main/java/com/example/coupon_core/repository/redis/dto/CouponRedisEntity.java
@@ -0,0 +1,48 @@
+package com.example.coupon_core.repository.redis.dto;
+
+import com.example.coupon_core.exception.CouponIssueException;
+import com.example.coupon_core.exception.ErrorCode;
+import com.example.coupon_core.model.Coupon;
+import com.example.coupon_core.model.CouponType;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+
+import static com.example.coupon_core.exception.ErrorCode.*;
+
+public record CouponRedisEntity(
+        Long id,
+        CouponType couponType,
+        Integer totalQuantity,
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueStart,
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueEnd
+) {
+    public CouponRedisEntity(Coupon coupon){
+        this(
+                coupon.getId(),
+                coupon.getCouponType(),
+                coupon.getTotalQuantity(),
+                coupon.getDateIssueStart(),
+                coupon.getDateIssueEnd()
+        );
+    }
+
+    private boolean availableIssueDate(){
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueStart.isAfter(now);
+    }
+
+    public void checkIssuableCoupon(){
+        if( !availableIssueDate()){
+            throw new CouponIssueException(FAIL_COUPON_ISSUE_REQUEST,"발급 가능한 일자가 아닙니다!");
+        }
+    }
+
+}
