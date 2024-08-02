2e118b223e696000aa84d95598b26fa0a3787470
minutaurus <made_power@naver.com>
2024-08-03 00:14:22 +0900

    Exception 리팩터링

--- coupon-core/src/main/java/com/example/coupon_core/service/CouponIssueRedisService.java
+++ coupon-core/src/main/java/com/example/coupon_core/service/CouponIssueRedisService.java
@@ -1,15 +1,28 @@
 package com.example.coupon_core.service;
 
+import com.example.coupon_core.exception.CouponIssueException;
 import com.example.coupon_core.repository.redis.RedisRepository;
+import com.example.coupon_core.repository.redis.dto.CouponRedisEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.example.coupon_core.exception.ErrorCode.DUPLICATED_COUPON_ISSUE;
+import static com.example.coupon_core.exception.ErrorCode.INVALID_COUPON_ISSUE_QUANTITY;
 import static com.example.coupon_core.util.CouponRedisUtils.getIssueRequestKey;
 
 @RequiredArgsConstructor
 @Service
 public class CouponIssueRedisService {
     private final RedisRepository redisRepository;
+
+    public void checkCouponIssueQuantity(CouponRedisEntity couponRedisEntity, long userId){
+        if(!availableTotalIssueQuantity(couponRedisEntity.totalQuantity(), couponRedisEntity.id())){
+            throw new CouponIssueException(INVALID_COUPON_ISSUE_QUANTITY,"발급 가능한 수량을 초과합니다.");
+        }
+        if(!availableUserIssueQuantity(couponRedisEntity.id(), userId)){
+            throw new CouponIssueException(DUPLICATED_COUPON_ISSUE,"이미 발급한 쿠폰입니다.");
+        }
+    }
 
     public boolean availableTotalIssueQuantity(Integer totalQuantity, long couponId){
         if(totalQuantity == null){
