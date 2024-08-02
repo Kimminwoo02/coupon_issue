9d06e4510d10b0b2bc8d0223a81b4f883e85fbff
minutaurus <made_power@naver.com>
2024-08-03 00:13:05 +0900

    Caching 리팩토링
    기존 코드에서 동시성을 해결함과 동시에 캐싱을 사용할 수 있도록 리팩터링 하였다.

--- coupon-core/src/main/java/com/example/coupon_core/service/AsyncCouponIssueServiceV1.java
+++ coupon-core/src/main/java/com/example/coupon_core/service/AsyncCouponIssueServiceV1.java
@@ -2,10 +2,10 @@ package com.example.coupon_core.service;
 
 import com.example.coupon_core.component.DistributeLockExecutor;
 import com.example.coupon_core.exception.CouponIssueException;
-import com.example.coupon_core.exception.ErrorCode;
 import com.example.coupon_core.model.Coupon;
 import com.example.coupon_core.repository.redis.RedisRepository;
 import com.example.coupon_core.repository.redis.dto.CouponIssueRequest;
+import com.example.coupon_core.repository.redis.dto.CouponRedisEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -23,23 +23,19 @@ public class AsyncCouponIssueServiceV1 {
     private final CouponIssueService couponIssueService;
     private final DistributeLockExecutor distributeLockExecutor;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final CouponCacheService couponCacheService;
+
+
+
     public void issue(long couponId, long userId){
         // 1. 유저의 요청을 sorted set에 적체
 //        String key = "issue.request.sortedset.couponId=%s".formatted(couponId);
 //        redisRepository.zAdd(key,String.valueOf(userId),System.currentTimeMillis());
 
-        Coupon coupon = couponIssueService.findCoupon(couponId);
-        if(!coupon.availableIssueDate()){
-            throw new CouponIssueException(INVALID_COUPON_ISSUE_DATE,"발급 기간이 지났습니다");
-        }
+        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
+        coupon.checkIssuableCoupon();
         distributeLockExecutor.execute("lock_%s".formatted(couponId),3000,3000,()->{
-            if(couponIssueRedisService.availableTotalIssueQuantity(coupon.getTotalQuantity(),couponId)){
-                throw new CouponIssueException(INVALID_COUPON_ISSUE_QUANTITY,"발급 가능한 수량을 초과합니다.");
-            }
-            if(couponIssueRedisService.availableUserIssueQuantity(couponId,userId)){
-                throw new CouponIssueException(DUPLICATED_COUPON_ISSUE,"이미 발급한 쿠폰입니다.");
-            }
-            
+            couponIssueRedisService.checkCouponIssueQuantity(coupon,userId);
             issueRequest(couponId,userId);
         });
 
