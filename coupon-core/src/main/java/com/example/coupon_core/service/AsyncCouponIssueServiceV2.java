cbe719556b3ec4901ff5ea08856da47068fda93e
minutaurus <made_power@naver.com>
2024-08-05 20:43:03 +0900

    RedisScript를 사용한 Serivce V2 생성

---
+++ coupon-core/src/main/java/com/example/coupon_core/service/AsyncCouponIssueServiceV2.java
@@ -0,0 +1,40 @@
+package com.example.coupon_core.service;
+
+import com.example.coupon_core.component.DistributeLockExecutor;
+import com.example.coupon_core.exception.CouponIssueException;
+import com.example.coupon_core.repository.redis.RedisRepository;
+import com.example.coupon_core.repository.redis.dto.CouponIssueRequest;
+import com.example.coupon_core.repository.redis.dto.CouponRedisEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.example.coupon_core.exception.ErrorCode.FAIL_COUPON_ISSUE_REQUEST;
+import static com.example.coupon_core.util.CouponRedisUtils.getIssueRequestKey;
+import static com.example.coupon_core.util.CouponRedisUtils.getIssueRequestQueueKey;
+
+@Service
+@RequiredArgsConstructor
+public class AsyncCouponIssueServiceV2 {
+    private final RedisRepository redisRepository;
+    private final CouponCacheService couponCacheService;
+
+
+
+    public void issue(long couponId, long userId){
+
+        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
+        coupon.checkIssuableCoupon();
+        issueRequest(couponId,userId, coupon.totalQuantity());
+    }
+
+    private void issueRequest(long couponId, long userId, Integer totalIssueQuantity){
+        if(totalIssueQuantity == null){
+            redisRepository.issueRequest(couponId, userId, Integer.MAX_VALUE);
+        }
+        redisRepository.issueRequest(couponId, userId, totalIssueQuantity);
+
+    }
+
+}
