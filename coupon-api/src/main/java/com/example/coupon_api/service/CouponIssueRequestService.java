b23b6dd052c9fb9d3af795887e757c2c8e132fbb
minutaurus <made_power@naver.com>
2024-08-07 20:06:32 +0900

    V2를 사용하도록 서비스와 컨트롤러 로직 추가

--- coupon-api/src/main/java/com/example/coupon_api/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/example/coupon_api/service/CouponIssueRequestService.java
@@ -3,6 +3,7 @@ package com.example.coupon_api.service;
 import com.example.coupon_api.controller.dto.CouponIssueRequestDto;
 import com.example.coupon_core.component.DistributeLockExecutor;
 import com.example.coupon_core.service.AsyncCouponIssueServiceV1;
+import com.example.coupon_core.service.AsyncCouponIssueServiceV2;
 import com.example.coupon_core.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,7 @@ public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
     private final DistributeLockExecutor distributeLockExecutor;
     private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
+    private final AsyncCouponIssueServiceV2 asyncCouponIssueServiceV2;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto){
 //        distributeLockExecutor.execute ("lock_name "+ requestDto.couponId(), 10000,10000, ()->{ 레디스 락 제거
@@ -25,6 +27,11 @@ public class CouponIssueRequestService {
 
     public void asyncIssueRequestV1(CouponIssueRequestDto requestDto){
         asyncCouponIssueServiceV1.issue(requestDto.couponId(), requestDto.userId());
+        log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    public void asyncIssueRequestV2(CouponIssueRequestDto requestDto){
+        asyncCouponIssueServiceV2.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
